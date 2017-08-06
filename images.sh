f4c431d86281c77bef0f5fc8e93f8f51275aec12
Owen Shepherd <414owen@gmail.com>
2017-08-06 16:42:58 +0100

    Bitmap support in image script

--- images.sh
+++ images.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-for i in images/*.svg; do 
-	out="images/min/$(printf "$(basename $i)" | sed "s/.svg//g" | sed "s/ /-/g" | tr '[:upper:]' '[:lower:]' | rev).svg"
-	echo "Cleaning \"$i\" -> \"$out\""
-	svgcleaner "$i" "$out"
+for i in images/*.*; do 
+	ending=$(printf "${i}" | sed "s/^.*\.//g")
+	base=$(printf "$(basename "${i}")" | sed "s/\.${ending}$//g")
+	echo "Base: \"${base}\", Ending: \"${ending}\""
+	outname="$(printf "${base}" | sed "s/ /-/g" | tr '[:upper:]' '[:lower:]' | rev).${ending}"
+	if [ "${ending}" == "svg" ]; then
+		out="images/min/${outname}"
+		if [ ! -f "${out}" ]; then
+			echo "Cleaning \"${base}.${ending}\" -> \"${outname}\""
+			svgcleaner "${i}" "${out}"
+		fi
+	else
+		out="images/bitmap/${outname}"
+		echo "Symlinking ${base}.${ending} -> ${outname}"
+		ln -s "${i}" "${out}"
+	fi
 done
